953af25a593b2319e3469e95cc3dc375ae5b13f1
MohamadFeeshar <mohamed.feeshar@gmail.com>
2020-03-14 00:46:08 +0200

    add relationships with models

--- app/Book.php
+++ app/Book.php
@@ -12,4 +12,20 @@ class Book extends Model
         return $query->where('title', 'like', '%' .$searchTerm. '%')
                      ->orWhere('author', 'like', '%' .$searchTerm. '%');
     }
+    
+    public function category(){
+        return $this->hasOne('App\Category');
+    }
+    
+    public function comments(){
+        return $this->hasMany('App\Comment');
+    }
+    
+    public function usersFavorites(){
+        return $this->hasMany(User::class, 'favorites');
+    }
+
+    public function usersLease(){
+        return $this->hasMany(User::class, 'leases');
+    }
 }
