4ad0e8cc5911f46395effc908274629c3b9372f8
basmmohamed <basmasalah2017@gmail.com>
2020-03-17 14:56:54 +0200

    fix navbar in user home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,57 +33,22 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('home') }}">
+                <!-- <a class="navbar-brand" href="{{ url('home') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> -->
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">MyBooks <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Favourites <span class="sr-only">(current)</span></a>
-                            </li>
-                    </ul>
+            
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        <div class="container">
-                        
-                        <span> Order By</span>
-                        <div class="col-sm-8">
-                                    <form action="order" method="GET">
-                                        {{csrf_field()}}
-                                        <div class="input-group">
-                                            <span class="input-group-btn">
-                                            <button type="submit" name="order" class="btn btn-secondary"value="created_at">Latest</button>
-                                            <button class="btn btn-secondary" name="order" type="submit" value="price">Price</button>
+                        @yield('nav')
 
-                                            </span>
-                                        </div>
-                                    </form>
-                                </div>
-                              
-                        </div>
-                            <div class="container">
-                                <div class="col-sm-8">
-                                    <form action="home" method="GET">
-                                        {{csrf_field()}}
-                                        <div class="input-group">
-                                            <input type="text" class="form-control" name="searchTerm" placeholder="Search by name or author for..." value="{{ isset($searchTerm) ? $searchTerm : '' }}">
-                                            <span class="input-group-btn">
-                                                <button class="btn btn-secondary" type="submit">Search</button>
-                                            </span>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
